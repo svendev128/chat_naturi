492a930b9d1a4ea86a0cded108cfffec4735a451
svendev128 <129780971+svendev128@users.noreply.github.com>
2023-12-19 08:41:24 +0900

    build

--- src/actions/types.jsx
+++
@@ -1,1 +0,0 @@
-export const ADD_ANSWER = "ADD_ANSWER";
--- src/reducers/index.jsx
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import auth from './auth';
-import question from './questions';
-
-export default combineReducers({
-    auth,
-    question
-});
